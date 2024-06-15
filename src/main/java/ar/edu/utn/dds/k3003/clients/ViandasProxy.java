2fcd4619dbe3589b078e62c7405f736865f95da9
OviedoFrancoAlexander <64283850+OviedoFranc@users.noreply.github.com>
2024-06-15 10:55:00 -0300

    Update ViandasProxy.java

--- src/main/java/ar/edu/utn/dds/k3003/clients/ViandasProxy.java
+++ src/main/java/ar/edu/utn/dds/k3003/clients/ViandasProxy.java
@@ -22,7 +22,7 @@ public class ViandasProxy implements FachadaViandas {
     public ViandasProxy(ObjectMapper objectMapper) {
 
         var env = System.getenv();
-        this.endpoint = env.getOrDefault("URL_VIANDAS", "https://two024-tp-entrega-2-danielbare12.onrender.com");
+        this.endpoint = env.getOrDefault("URL_VIANDAS", "https://two024-tp-entrega-2-hiogen.onrender.com");
 
         var retrofit =
                 new Retrofit.Builder()
