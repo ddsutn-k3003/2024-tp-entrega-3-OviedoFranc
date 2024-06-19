c8409011b67e36d97a1441de8e2d136ded541f7e
OviedoFranc <oviedofranc@gmail.com>
2024-06-19 03:39:22 -0300

    Try fix bug

--- src/main/java/ar/edu/utn/dds/k3003/controller/HeladeraController.java
+++ src/main/java/ar/edu/utn/dds/k3003/controller/HeladeraController.java
@@ -9,10 +9,8 @@ import ar.edu.utn.dds.k3003.model.DTO.GetErrorHeladeraDTO;
 import ar.edu.utn.dds.k3003.utils.utilsHeladera;
 import io.javalin.http.Context;
 import io.javalin.http.HttpStatus;
-import org.jetbrains.annotations.Debug;
 import org.jetbrains.annotations.NotNull;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.NoSuchElementException;
 
